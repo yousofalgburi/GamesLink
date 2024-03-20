86fb1bcb0261163da3bacf1a4f66fb27c5c587de
Yousof Algburi <yousofalgburi@gmail.com>
2024-03-20 19:50:15 -0400

    pimping UI

--- components/LinkRoom.tsx
+++ components/LinkRoom.tsx
@@ -11,7 +11,18 @@ import { useToast } from './ui/use-toast'
 import { UserInRoom } from '@/types/linkroom'
 import { Alert, AlertDescription, AlertTitle } from './ui/alert'
 import { GamepadIcon } from 'lucide-react'
-import { Room } from '@prisma/client'
+import { Room, User } from '@prisma/client'
+import { Switch } from './ui/switch'
+import { Label } from './ui/label'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
 
 export default function LinkRoom({
     roomId,
@@ -27,6 +38,7 @@ export default function LinkRoom({
     const { toast } = useToast()
     const [usersInRoom, setUsersInRoom] = useState<UserInRoom[]>(roomUsers)
     const [userLowGameCount, setUserLowGameCount] = useState(false)
+    const [waitList, setWaitList] = useState<User[]>([])
 
     const ws = useMemo(() => {
         return new WebSocket(`ws://localhost:8000`)
@@ -73,7 +85,55 @@ export default function LinkRoom({
             )}
 
             <div className="grid items-start gap-8">
-                <h1 className="text-3xl font-bold md:text-4xl">Link Room ({usersInRoom.length}/10)</h1>
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-2">
+                        <h1 className="text-3xl font-bold md:text-4xl">Link Room ({usersInRoom.length}/10)</h1>
+                        <p className="truncate text-sm text-muted-foreground">
+                            Created on {roomDetails.createdAt.toLocaleString()}
+                        </p>
+                    </div>
+
+                    <div className="flex gap-4">
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button variant="outline">Show Room Queue</Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[425px]">
+                                <DialogHeader>
+                                    <DialogTitle>Users Wait List</DialogTitle>
+                                    <DialogDescription>Users waiting to join room.</DialogDescription>
+                                </DialogHeader>
+                                <div className="grid gap-4 py-4">
+                                    {waitList.length ? (
+                                        waitList.map((user) => (
+                                            <div key={user.name} className="flex items-center justify-between">
+                                                <div className="flex items-center gap-2">
+                                                    <UserAvatar user={user} />
+                                                    <Label htmlFor="name">{user.name}</Label>
+                                                </div>
+
+                                                <div className="flex gap-2">
+                                                    <Button size="sm" disabled>
+                                                        Accept
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <Label>No users in queue</Label>
+                                    )}
+                                </div>
+                            </DialogContent>
+                        </Dialog>
+
+                        <div className="flex items-center space-x-2">
+                            <Switch id="airplane-mode" />
+                            <Label htmlFor="airplane-mode">Public</Label>
+                        </div>
+
+                        <Button>Roll!</Button>
+                    </div>
+                </div>
 
                 <div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
                     {usersInRoom.map((user, index) => (
