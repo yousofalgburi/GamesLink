472fbfbea896408412042bd8c0515926e8ad9dd3
Yousof Algburi <yousofalgburi@gmail.com>
2024-06-15 20:30:28 -0400

    server setup with rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/api',
+				destination: 'http://127.0.0.1:8787/'
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig
