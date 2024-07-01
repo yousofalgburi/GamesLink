a839ebf5d4b1de8a901bda793b4e49b59b908628
Yousof Algburi <yousofalgburi@gmail.com>
2024-07-01 18:57:12 -0400

    remove auth

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,8 +1,4 @@
 export async function GET(req, res) {
-	if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-		return res.status(401).end('Unauthorized')
-	}
-
 	const result = await fetch('http://worldtimeapi.org/api/timezone/America/Chicago', {
 		cache: 'no-store',
 	})
