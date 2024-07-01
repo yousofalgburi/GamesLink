18b40c57159a74d7a58bd737387898259733f89e
Yousof Algburi <yousofalgburi@gmail.com>
2024-07-01 18:53:15 -0400

    secure cron work

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,4 +1,8 @@
-export async function GET() {
+export async function GET(req, res) {
+	if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+		return res.status(401).end('Unauthorized')
+	}
+
 	const result = await fetch('http://worldtimeapi.org/api/timezone/America/Chicago', {
 		cache: 'no-store',
 	})
