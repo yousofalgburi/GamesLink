ef3e902cd38a880055f70f22824a0de780955b35
Yousof Algburi <yousofalgburi@gmail.com>
2024-07-07 12:14:25 -0400

    small vote fix

--- components/game-vote/GameVoteServer.tsx
+++ components/game-vote/GameVoteServer.tsx
@@ -1,44 +1,39 @@
 import { getAuthSession } from '@/lib/auth'
-import type { ProcessedGame, GameInteraction, Vote } from '@prisma/client'
+import type { ProcessedGame, Vote } from '@prisma/client'
 import { notFound } from 'next/navigation'
 import GameVoteClient from './GameVoteClient'
-import { db } from '@/lib/db'
 import type { VoteType } from '@/constants/enums'
 
 interface GameVoteServerProps {
 	gameId: string
 	initialVotesAmt?: number
-	initialVote?: Vote['type'] | null
-	getData?: () => Promise<{ game: ProcessedGame | null; gameInteraction: GameInteraction | null }>
+	initialVote?: VoteType | null
+	getData?: () => Promise<(ProcessedGame & { votes: Vote[] }) | null>
 }
 
 const GameVoteServer = async ({ gameId, initialVotesAmt, initialVote, getData }: GameVoteServerProps) => {
 	const session = await getAuthSession()
 
 	let _votesAmt = 0
-	let _currentVote: Vote['type'] | null | undefined = undefined
+	let _currentVote: VoteType | null | undefined = undefined
 
 	if (getData) {
-		const { game, gameInteraction } = await getData()
-		if (!game) return notFound()
+		const post = await getData()
+		if (!post) return notFound()
 
-		_votesAmt = gameInteraction?.voteCount ?? 0
+		_votesAmt = post.votes.reduce((acc, vote) => {
+			if (vote.type === 'UP') return acc + 1
+			if (vote.type === 'DOWN') return acc - 1
+			return acc
+		}, 0)
 
-		if (gameInteraction && session?.user?.id) {
-			const userVote = await db.vote.findFirst({
-				where: {
-					gameId: gameInteraction.id,
-					userId: session.user.id,
-				},
-			})
-			_currentVote = userVote?.type
-		}
+		_currentVote = post.votes.find((vote) => vote.userId === session?.user?.id)?.type as VoteType
 	} else {
 		_votesAmt = initialVotesAmt ?? 0
 		_currentVote = initialVote
 	}
 
-	return <GameVoteClient gameId={gameId} initialVotesAmt={_votesAmt} initialVote={_currentVote as VoteType} />
+	return <GameVoteClient gameId={gameId} initialVotesAmt={_votesAmt} initialVote={_currentVote} />
 }
 
 export default GameVoteServer
