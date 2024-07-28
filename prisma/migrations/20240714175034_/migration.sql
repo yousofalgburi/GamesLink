b139e8a7db090b5669f84e6f40e26b8d214205d4
Yousof Algburi <yousofalgburi@gmail.com>
2024-07-28 18:09:16 -0400

    migrate off libsql

--- prisma/migrations/20240714175034_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "ProcessedGame_type_requiredAge_idx";
-
--- CreateIndex
-CREATE INDEX "ProcessedGame_type_idx" ON "ProcessedGame"("type");
--- prisma/migrations/20240714181534_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "ProcessedGame_type_idx";
-
--- CreateIndex
-CREATE INDEX "ProcessedGame_type_voteCount_idx" ON "ProcessedGame"("type", "voteCount");
