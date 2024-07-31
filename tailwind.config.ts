89fb1f86636f347ee19997e96da6f6c81d7117bc
Yousof Algburi <yousofalgburi@gmail.com>
2024-07-31 15:06:41 -0400

    fix styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,14 @@
-import type { Config } from 'tailwindcss'
-const { fontFamily } = require('tailwindcss/defaultTheme')
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
-const config: Config = {
-	darkMode: ['class'],
-	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{ts,tsx}'],
+export default {
+	content: ['./components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px',
-			},
-		},
 		extend: {
+			fontFamily: {
+				heading: ['var(--font-heading)', ...fontFamily.sans],
+				body: ['var(--font-body)', ...fontFamily.sans],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
@@ -50,34 +45,26 @@ const config: Config = {
 				},
 			},
 			borderRadius: {
+				xl: 'calc(var(--radius) + 4px)',
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)',
 			},
 			keyframes: {
 				'accordion-down': {
-					from: { height: '0' },
+					from: { height: 0 },
 					to: { height: 'var(--radix-accordion-content-height)' },
 				},
 				'accordion-up': {
 					from: { height: 'var(--radix-accordion-content-height)' },
-					to: { height: '0' },
+					to: { height: 0 },
 				},
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
 			},
-			fontFamily: {
-				sans: ['var(--font-sans)', ...fontFamily.sans],
-			},
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
 		},
 	},
 	plugins: [require('tailwindcss-animate')],
 }
-
-export default config
