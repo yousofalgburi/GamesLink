0768e70150628e75f1d0f053d44a9a3d7aa83783
Yousof Algburi <yousofalgburi@gmail.com>
2024-08-17 17:31:47 -0400

    fix issue with redis client

--- lib/redis.ts
+++ lib/redis.ts
@@ -3,12 +3,22 @@ import { createClient } from 'redis'
 let redis: ReturnType<typeof createClient> | null = null
 
 export async function getRedisClient() {
-	if (!redis) {
+	if (!redis || !redis.isOpen) {
 		redis = createClient({
 			url: process.env.REDIS_URL,
 		})
-		redis.on('error', (err) => console.log('Redis Client Error', err))
+		redis.on('error', (err) => {
+			console.error('Redis Client Error', err)
+			redis = null
+		})
 		await redis.connect()
 	}
 	return redis
+}
+
+export async function closeRedisConnection() {
+	if (redis?.isOpen) {
+		await redis.quit()
+		redis = null
+	}
 }
