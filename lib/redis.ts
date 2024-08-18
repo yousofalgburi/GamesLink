2d2f039fc53c0c4f8675b48ce9e443af7d888737
Yousof Algburi <yousofalgburi@gmail.com>
2024-08-18 08:01:32 -0400

    fix redis crashing

--- lib/redis.ts
+++ lib/redis.ts
@@ -7,9 +7,8 @@ export async function getRedisClient() {
 		redis = createClient({
 			url: process.env.REDIS_URL,
 		})
-		redis.on('error', (err) => {
-			console.error('Redis Client Error', err)
-			redis = null
+		redis.on('error', (error) => {
+			console.error('Redis Client Error:', error)
 		})
 		await redis.connect()
 	}
