97dcdb68a0455024c79cfbbfe1da7e044558e710
Yousof Algburi <yousofalgburi@gmail.com>
2024-08-31 15:49:46 -0400

    limit to 100k games in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 import { db } from '@/db'
 import { processedGames } from '@/db/schema'
-import { and, eq } from 'drizzle-orm'
+import { and, desc, eq } from 'drizzle-orm'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const baseUrl = 'https://gameslink.app'
@@ -9,12 +9,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 		.select({ steamAppid: processedGames.steamAppid })
 		.from(processedGames)
 		.where(and(eq(processedGames.nsfw, false), eq(processedGames.type, 'game')))
-		.limit(10000)
+		.orderBy(desc(processedGames.releaseDate))
+		.limit(100000)
 
 	const gameRoutes = gameIds.map(({ steamAppid }) => ({
 		url: `${baseUrl}/game/${steamAppid}`,
 		lastModified: new Date(),
-		priority: 0.5,
 	}))
 
 	return [
@@ -22,25 +22,21 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 			url: baseUrl,
 			lastModified: new Date(),
 			changeFrequency: 'daily',
-			priority: 1,
 		},
 		{
 			url: `${baseUrl}/home`,
 			lastModified: new Date(),
 			changeFrequency: 'daily',
-			priority: 0.8,
 		},
 		{
 			url: `${baseUrl}/my-games`,
 			lastModified: new Date(),
 			changeFrequency: 'daily',
-			priority: 0.8,
 		},
 		{
 			url: `${baseUrl}/link-room/new`,
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.6,
 		},
 		...gameRoutes,
 	]
