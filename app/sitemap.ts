9738f458e40150185660d015e949b0906a54878c
Yousof Algburi <yousofalgburi@gmail.com>
2024-08-31 15:47:29 -0400

    limit to 10k games in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,28 +9,38 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 		.select({ steamAppid: processedGames.steamAppid })
 		.from(processedGames)
 		.where(and(eq(processedGames.nsfw, false), eq(processedGames.type, 'game')))
+		.limit(10000)
 
 	const gameRoutes = gameIds.map(({ steamAppid }) => ({
 		url: `${baseUrl}/game/${steamAppid}`,
 		lastModified: new Date(),
+		priority: 0.5,
 	}))
 
 	return [
 		{
 			url: baseUrl,
 			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 1,
 		},
 		{
 			url: `${baseUrl}/home`,
 			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.8,
 		},
 		{
 			url: `${baseUrl}/my-games`,
 			lastModified: new Date(),
+			changeFrequency: 'daily',
+			priority: 0.8,
 		},
 		{
 			url: `${baseUrl}/link-room/new`,
 			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.6,
 		},
 		...gameRoutes,
 	]
